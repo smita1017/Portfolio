499bb28fafd0fb62bb69d5dfbcade26a84a78e25
smita1017 <smita126@outlook.com>
2025-01-25 18:29:33 +0530

    Create Button.jsx

---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = (props) => {
+  return (
+    <div>
+        <button className='btnDesign'>{props.name}</button>
+    </div>
+
+  )
+}
+
+export default Button
