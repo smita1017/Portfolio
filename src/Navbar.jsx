6c0820a55f42c52e1cc0f2d4404ccc22d8c638c3
smita1017 <smita126@outlook.com>
2025-01-25 18:24:10 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,1 +1,39 @@
 
+import React from 'react'
+import { NavLink } from 'react-router'
+import download from '../assets/download.png'
+import Button from './Button'
+const NavBar = (props) => {
+
+  return (
+    <div className='navContainer'>
+      <div className="logo">
+       <h1 className='logoimg'>Smita</h1>
+      </div>
+
+      <ul className='links'>
+          
+          <li>
+            <NavLink className="link" to="/home">Home</NavLink>
+          </li>
+          <li>
+            <NavLink className="link" to="/about">About Me!</NavLink>
+          </li>
+          <li>
+            <NavLink className="link" to="/services">Services</NavLink>
+          </li>
+          <li>
+            <NavLink className="link" to="/portfolio">Portfolio</NavLink>
+          </li>
+          <li>
+            <NavLink className="link" to="/contact">Contact</NavLink>
+          </li>
+      </ul>
+      <div className="cta-container">
+        <Button name="Connect Me"/>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
